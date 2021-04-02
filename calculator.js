8437c268dd832b6348ad4428e6e7438ba5221464
Aayush Jha <aayushjha48@gmail.com>
2021-04-02 22:27:26 +0530

    WD :- BimCalculator is Done

---
+++ calculator.js
@@ -0,0 +1,47 @@
+//jshint esversion:6
+
+//This part is for index.html section
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true }));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+
+  var num1 = Number(req.body.n1);
+  var num2 = Number(req.body.n2);
+
+  var result = num1 + num2;
+
+  res.send("Result of the calculation is:" + result);
+});
+
+
+
+
+//This part is for bmiCalculator.html
+
+app.get("/bmicalculator", function(req,res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req,res) {
+
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+
+  var result1 = weight / (height*height) ;
+
+  res.send("Your BMI is" + result1);
+
+});
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000.");
+});
